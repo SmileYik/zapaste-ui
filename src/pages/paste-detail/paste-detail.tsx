0d4247614fa2e713076599045d2bbb3b4678435d
SmileYik <miskyle@outlook.com>
2026-01-07 08:59:30 +0800

    chore: clean paste detail page

--- src/pages/paste-detail/paste-detail.tsx
+++ src/pages/paste-detail/paste-detail.tsx
@@ -1,16 +1,10 @@
-import { solarizedLight as codeStyle } from "react-syntax-highlighter/dist/esm/styles/hljs";
-import { MdDialog, MdDivider, MdElevation, MdFilledButton, MdFilledTextField, MdFilledTonalButton, MdIcon, MdOutlinedButton, MdOutlinedIconButton, MdOutlinedTextField } from "../../components/Material";
-import type PasteModel from "../../entity/paste_model";
+import { MdDialog, MdFilledButton, MdFilledTonalButton, MdOutlinedTextField } from "../../components/Material";
 import styles from "./paste-detail.module.css"
-import { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
-import hljs from 'highlight.js';
-import { calendar_today, content_copy, edit_document, lock, visibility, visibility_off } from "../../components/Icons";
-import FileList from "../../components/file-list/file-list";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getLockedPaste } from "../../api";
 import PatseModelDetail, { PatseModelDetailSkeleton } from "../../components/paste-model-detail/paste-model-detail";
-import { useLocation, useMatches, useNavigate } from "react-router";
+import { useMatches, useNavigate } from "react-router";
 
 export default function PatseDetail({
 
@@ -19,14 +13,23 @@ export default function PatseDetail({
     const route = matches[matches.length - 1];
     const navigate = useNavigate();
 
-    const [name, setName] = useState(route.params?.name || "");
+    const gotoChoosePastePage = useCallback(() => {
+        navigate(route.pathname + "/..")
+    }, [])
+
+    const name = route.params?.name || "";
     const [password, setPassword] = useState("");
     const [passwordField, setPasswordField] = useState("");
     const [errorTimes, setErrorTimes] = useState(0);
 
     const isEmptyName = useMemo(() => {
         return (name || "").trim().length === 0;
-    }, [name]);
+    }, []);
+
+    if (isEmptyName) {
+        gotoChoosePastePage();
+        return (<></>)
+    }
 
     const {isPending, error, data, isError} = useQuery({
         queryKey: ["data", name, password],
@@ -46,10 +49,6 @@ export default function PatseDetail({
         }
         setPassword(passwordField);
     }, [passwordField]);
-
-    const gotoChoosePastePage = useCallback(() => {
-        navigate(route.pathname + "/..")
-    }, [])
 
     return (
         <div className={styles["detail-container"]}>
@@ -92,36 +91,36 @@ interface PatseDetailProps {
     
 }
 
-function PasteNameInput({
-    name = "",
-    onChange,
-}: PasteNameInputProps) {
-    const [currentName, setCurrentName] = useState(name)
-    return (
-        <div>
-            <form className={styles["request-paste-name"]} method="dialog">
-                <MdElevation/>
+// function PasteNameInput({
+//     name = "",
+//     onChange,
+// }: PasteNameInputProps) {
+//     const [currentName, setCurrentName] = useState(name)
+//     return (
+//         <div>
+//             <form className={styles["request-paste-name"]} method="dialog">
+//                 <MdElevation/>
 
-                <MdOutlinedTextField
-                    label="剪切板名"
-                    placeholder="请输入剪切板名"
-                    required
-                    defaultValue={currentName}
-                    onChange={(target: any) => setCurrentName(target.target.value)}
-                    style={{
-                        width: "100%"
-                    }}
-                ></MdOutlinedTextField>
+//                 <MdOutlinedTextField
+//                     label="剪切板名"
+//                     placeholder="请输入剪切板名"
+//                     required
+//                     defaultValue={currentName}
+//                     onChange={(target: any) => setCurrentName(target.target.value)}
+//                     style={{
+//                         width: "100%"
+//                     }}
+//                 ></MdOutlinedTextField>
 
-                <div className={styles["request-paste-name-actions"]}>
-                    <MdFilledButton value="ok" onClick={() => onChange(currentName)}>确定</MdFilledButton>
-                </div>
-            </form>
-        </div>
-    );
-}
+//                 <div className={styles["request-paste-name-actions"]}>
+//                     <MdFilledButton value="ok" onClick={() => onChange(currentName)}>确定</MdFilledButton>
+//                 </div>
+//             </form>
+//         </div>
+//     );
+// }
 
-interface PasteNameInputProps {
-    name?: string,
-    onChange: (name: string) => void
-}
+// interface PasteNameInputProps {
+//     name?: string,
+//     onChange: (name: string) => void
+// }
